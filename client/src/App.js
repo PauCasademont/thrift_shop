6419009d8a750e63d970f6deeae48e01e9cdeb38
PauCasademont <u1953627@campus.udg.edu>
2021-01-26 19:05:33 +0100

    Added Browser Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+ 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import useStyles from './styles';
+
+function Auth() {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            <h1>Auth Page</h1>
+        </div>
+    )
+}
+
+export default Auth
