a906db326bd8e7b5779fe1ffd7dbc0bf6ff45a7a
PauCasademont <u1953627@campus.udg.edu>
2021-01-26 20:45:11 +0100

    Added Form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Container, Paper, Grid, TextField, Box } from '@material-ui/core';
 
 import useStyles from './styles';
 
 function Auth() {
     const classes = useStyles();
-    
-    return (
-        <div>
-            <h1>Auth Page</h1>
-        </div>
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    return (    
+        <Container maxWidth="sm">
+            <Paper className={classes.paper} elevation={3}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <TextField name="usename" onChange={handleChange} required fullWidth label="Username" autoFocus type="text"/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField name="password" onChange={handleChange} required fullWidth label="Password" type="password"/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField name="password2" onChange={handleChange} required fullWidth label="Repeat Password" type="password"/>
+                    </Grid>
+                </Grid>
+            </Paper>             
+        </Container>
     )
 }
 
--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -1,5 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({
-
+export default makeStyles((theme) => ({    
+    paper: {    
+        marginTop: theme.spacing(8), 
+        padding: theme.spacing(5),
+        paddingLeft: theme.spacing(10),
+        paddingRight: theme.spacing(10),
+    },
 }));
