b50445fba0dfed7aec03c378af0914b1c76cefb9
PauCasademont <u1953627@campus.udg.edu>
2021-01-27 13:09:41 +0100

    Added errors auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,21 +2,24 @@ import swal from 'sweetalert';
 
 import * as api from '../api/index.js';
 
+const createAlert = (title, text, icon, button) => {
+    swal({ title, text, icon, button });
+}
+
 export const signUp = async (form, router) => {
 
     if (form.password !== form.password2){
-        swal({
-            title: "Error",
-            text: "Passwords do not match",
-            icon: "error"
-        });       
+        createAlert("Error", "Passwords do not match", "error", "ok");      
     }
+
     else {
         try{
-            await api.req_singUp(form);                    
-            router.push('/')
+            await api.req_singUp(form);         
+            router.push('/');
         } catch (error) {
             console.log(error);
+            const message = error.response.data.message || "Something went wrong";
+            createAlert("Error", message, "error", "ok"); 
         }
     }
 }; 
